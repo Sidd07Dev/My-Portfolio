dbbee5e01b1b7d3e721c306d0b6548b1313cd33b
Sidhanta Pradhan <sidd07dev@gmail.com>
2024-06-26 13:15:21 +0530

    new changes

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -86,7 +86,7 @@ const Hero = () => {
             <img
               data-aos="fade-up"
               className=" h-[90%]  w-full object-cover md:h-[95%] md:m-auto sm:m-0"
-              src="https://sidhantapradhan.cmas.co.in/assets/profile-pic.png"
+              src="https://sidhantapradhan.cmas.co.in/assets/about-pic.png"
               // src={mine}
               alt="mine"
             />
